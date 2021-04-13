f86863029392009811acd48f9f731ebd5ac5cd88
Miguel Camba <miguel.camba@gmail.com>
2021-04-14 01:49:23 +0200

    Remove intl entirely

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,15 +1,3 @@
-<script context="module">
-	import { addMessages, init } from 'precompile-intl-runtime';
-    import en from '../../locales/en.json';
-	// @ts-ignore
-    addMessages('en', en)
-
-    init({
-		fallbackLocale: 'en',
-		initialLocale: 'en'
-	});
-</script>
-
 <script>
 	import '../app.css';
 </script>
