5782cda47c362584695fe018a00e9cce24d05cf1
Miguel Camba <miguel.camba@gmail.com>
2021-04-16 14:54:15 +0200

    Added more translations

--- src/lib/Translations.svelte
+++ src/lib/Translations.svelte
@@ -3,6 +3,7 @@
     import { t } from 'svelte-intl-precompile';
     export let gender;
     export let count;
+    export let ssr = false;
 	beforeUpdate(() => {
 		console.time('Render time')
 	});
@@ -15,9 +16,11 @@
 <p>{$t('plain')}</p>
 <p>{$t('interpolated', { values: { count: 123 }})}</p>
 <p>{$t('time', { values: { now: now } })}</p>
+<p>{$t('time-custom-format', { values: { now: now } })}</p>
 <p>{$t('date', { values: { today: now } })}</p>
+<p>{$t('date-custom-format', { values: { today: now } })}</p>
 <p>{$t('number', { values: { n: 123456789 } })}</p>
-<p>{$t('number', { values: { n: 1.34 } })}</p>
+<p>{$t('percent', { values: { n: 0.134 } })}</p>
 <p>{$t('pluralized', { values: { count: 2 } })}</p>
 <p>{$t('pluralized-with-hash', { values: { count } })}</p>
 <p>{$t('selected', { values: { gender } })}</p>
--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,14 +1,25 @@
 <script context="module">
 	import { addMessages, init } from 'svelte-intl-precompile';
     import en from '../../locales/en.js';
-    import es from '../../locales/es.js';
+    // import es from '../../locales/es.js';
 	// @ts-ignore
     addMessages('en', en)
-    addMessages('es', es)
+    // addMessages('es', es)
 
     init({
 		fallbackLocale: 'en',
-		initialLocale: 'en'
+		initialLocale: 'en',
+		formats: {
+			time: {
+				hour: { hour: 'numeric' }
+			},
+			date: {
+				'abbr-full': { month: 'short', day: 'numeric', year: 'numeric' }
+			},
+			number: {
+				percent: { style: 'percent' }
+			}			
+		}
 	});
 </script>
 
